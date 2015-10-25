78aca5eea20b91f33ef47fb9754f7b2c3cf89dd6
Cosmin Cioacla <tehciolo@gmail.com>
2015-10-25 11:34:02 +0200

    Prev commit fix

---
+++ assets/js/siteData.js
@@ -0,0 +1,13 @@
+module.exports = function() {
+  return {
+    menu: {
+      name: "menu",
+      next: "Next",
+      prev: "Previous",
+      jump: "Jump to page",
+      search: "Go!",
+      toc: "Table of Contents",
+      help: "Help"
+    }
+  }
+} 
