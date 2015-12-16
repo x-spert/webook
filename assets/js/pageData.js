61293647ddb413f60d311ce7c1ec7b951d5d2ff6
Cosmin Cioacla <tehciolo@gmail.com>
2015-12-16 23:36:36 +0200

    Added typer exercises

--- assets/js/pageData.js
+++ assets/js/pageData.js
@@ -10,7 +10,7 @@ module.exports = function() {
         data: [
           {
             identifier: 'one',
-            position: 'top: 9.5%; left: 37%',
+            position: 'top: 9.5%; left: 35%',
             model: '',
             solution: 'kalt'
           },
@@ -55,6 +55,211 @@ module.exports = function() {
             position: 'top: 86.5%; left: 33%',
             model: '',
             solution: 'kalt'
+          }
+        ]
+      }
+    },
+    {no: 10},{no: 11},{no: 12},{no: 13},
+    {
+      no: 14,
+      title: 'Sieh die Bilder an und antworte!',
+      ex: {
+        name: 'typer',
+        bg: '14ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 74%; left: 40%',
+            model: '',
+            solution: 'regnerisch'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 74%; left: 70%',
+            model: '',
+            solution: 'windig'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 78.5%; left: 40%',
+            model: '',
+            solution: 'es schneit'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 78.5%; left: 70%',
+            model: '',
+            solution: 'es ist kalt'
+          },
+          {
+            identifier: 'five',
+            position: 'top: 83%; left: 40%',
+            model: '',
+            solution: 'es ist wolkig'
+          },
+          {
+            identifier: 'six',
+            position: 'top: 83%; left: 70%',
+            model: '',
+            solution: 'warm'
+          },
+          {
+            identifier: 'seven',
+            position: 'top: 87.5%; left: 40%',
+            model: '',
+            solution: 'es ist sonnig'
+          },
+          {
+            identifier: 'eight',
+            position: 'top: 87.5%; left: 70%',
+            model: '',
+            solution: 'heiß'
+          }
+        ]
+      }
+    },
+    {
+      no: 15,
+      title: 'Antworte!',
+      ex: {
+        name: 'typer',
+        bg: '15ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 12%; left: 11.5%',
+            model: '',
+            solution: 'brauche einen schirm'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 38%; left: 11.5%',
+            model: '',
+            solution: 'ziehe die stiefel an'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 63.5%; left: 11.5%',
+            model: '',
+            solution: 'gehe nach hause'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 89.5%; left: 10.5%',
+            model: '',
+            solution: 'im herbst ist es neblig und windig'
+          }
+        ]
+      }
+    },
+    {no: 16},{no: 17},{no: 18},{no: 19},{no: 20},{no: 21},{no: 22},
+    {
+      no: 23,
+      title: 'Wo sind die Lebensmittel? Ergänze!',
+      ex: {
+        name: 'typer',
+        bg: '23ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 10%; left: 27.5%',
+            model: '',
+            solution: 'kühlschrank'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 19%; left: 27.5%',
+            model: '',
+            solution: 'pfanne'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 28.5%; left: 27.5%',
+            model: '',
+            solution: 'backofen'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 38%; left: 27.5%',
+            model: '',
+            solution: 'schüssel'
+          },
+          {
+            identifier: 'five',
+            position: 'top: 47.5%; left: 27.5%',
+            model: '',
+            solution: 'boden'
+          },
+          {
+            identifier: 'six',
+            position: 'top: 57%; left: 27.5%',
+            model: '',
+            solution: 'teller'
+          },
+          {
+            identifier: 'seven',
+            position: 'top: 66.5%; left: 27.5%',
+            model: '',
+            solution: 'glas'
+          },
+          {
+            identifier: 'eight',
+            position: 'top: 76%; left: 27.5%',
+            model: '',
+            solution: 'flasche'
+          },
+          {
+            identifier: 'nine',
+            position: 'top: 85.5%; left: 58.5%',
+            model: '',
+            solution: 'tablett'
+          }
+        ]
+      }
+    },
+    {no: 24},{no: 25},{no: 26},
+    {
+      no: 27,
+      title: 'Hör zu und schreib die Namen!',
+      ex: {
+        name: 'typer',
+        bg: '27ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 17.5%; left: 21.5%',
+            model: '',
+            solution: 'paul'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 39.5%; left: 21.5%',
+            model: '',
+            solution: 'sandra'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 63%; left: 21.5%',
+            model: '',
+            solution: 'stefan'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 17.5%; left: 65.5%',
+            model: '',
+            solution: 'marie'
+          },
+          {
+            identifier: 'five',
+            position: 'top: 39.5%; left: 65.5%',
+            model: '',
+            solution: 'robert'
+          },
+          {
+            identifier: 'six',
+            position: 'top: 63%; left: 65.5%',
+            model: '',
+            solution: 'lara'
           }
         ]
       }
