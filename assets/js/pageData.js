79c0284af651d8c83f578e66130ad62a89519491
Cosmin Cioacla <tehciolo@gmail.com>
2016-01-22 23:01:09 +0200

    ten exercises done

--- assets/js/pageData.js
+++ assets/js/pageData.js
@@ -90,11 +90,129 @@ module.exports = function() {
       }
     },
     {
+      no: 22,
+      title: 'Lies, schreib dann die Namen der Gebäude und zeichne den Weg!',
+      exTrigger: {
+        bottom: '35%',
+        top: '8%'
+      },
+      ex: {
+        name: 'typer',
+        bg: '22ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 12.8%; left: 33.3%; width: 25%',
+            model: '',
+            solution: 'schule'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 13.2%; left: 59.8%; width: 14%',
+            model: '',
+            solution: 'apotheke'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 13.2%; left: 79.3%; width: 15%',
+            model: '',
+            solution: 'postamt'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 12.8%; left: 10.3%; width: 15%',
+            model: '',
+            solution: 'park'
+          },
+          {
+            identifier: 'five',
+            position: 'top: 44.2%; left: 30.3%; width: 22%',
+            model: '',
+            solution: 'supermarkt'
+          },
+          {
+            identifier: 'six',
+            position: 'top: 60.8%; left: 30.8%; width: 21%',
+            model: '',
+            solution: 'schwimmbad'
+          },
+          {
+            identifier: 'seven',
+            position: 'top: 61.8%; left: 78.3%; width: 15%',
+            model: '',
+            solution: 'kirche'
+          }
+        ]
+      }
+    },
+    {
       no: 24,
       static: 'pg24ex4',
       staticTrigger: {
         top: '9%',
         bottom: '45%'
+      }
+    },
+    {
+      no: 25,
+      title: 'Ergänze die Formen der Verben müssen und wollen!',
+      exTrigger: {
+        bottom: '26%',
+        top: '44%'
+      },
+      ex: {
+        name: 'typer',
+        bg: '25ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 27.8%; left: 9.3%; width: 16%',
+            model: '',
+            solution: 'muss'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 33.8%; left: 14.3%; width: 18%',
+            model: '',
+            solution: 'will oder muss'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 39.8%; left: 14%; width: 25%',
+            model: '',
+            solution: 'müssen'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 45.5%; left: 6.3%; width: 23%',
+            model: '',
+            solution: 'wollen'
+          },
+          {
+            identifier: 'five',
+            position: 'top: 50.8%; left: 19.3%; width: 16%',
+            model: '',
+            solution: 'muss'
+          },
+          {
+            identifier: 'six',
+            position: 'top: 50.8%; left: 65.3%; width: 16%',
+            model: '',
+            solution: 'will'
+          },
+          {
+            identifier: 'seven',
+            position: 'top: 61.8%; left: 9.3%; width: 27%',
+            model: '',
+            solution: 'wollt oder müsst'
+          },
+          {
+            identifier: 'eight',
+            position: 'top: 67.8%; left: 5.3%; width: 16%',
+            model: '',
+            solution: 'müsst'
+          }
+        ]
       }
     },
     {
@@ -126,6 +244,182 @@ module.exports = function() {
       static3Trigger: {
         top: '55%',
         bottom: '8%'
+      }
+    },
+    {
+      no: 30,
+      title: 'Hör zu, schreib die Namen und sing!',
+      exTrigger: {
+        bottom: '4%',
+        top: '8%'
+      },
+      ex: {
+        name: 'typer',
+        bg: '30ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 28.8%; left: 68%; width: 9%',
+            model: '',
+            solution: 'anja'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 38%; left: 52%; width: 9%',
+            model: '',
+            solution: 'samir'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 57.7%; left: 66.5%; width: 9%',
+            model: '',
+            solution: 'jan'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 65.1%; left: 52%; width: 9%',
+            model: '',
+            solution: 'yuko'
+          }
+        ]
+      }
+    },
+    {
+      no: 31,
+      title: 'Ergänze!',
+      exTrigger: {
+        bottom: '3%',
+        top: '62%'
+      },
+      ex: {
+        name: 'typer',
+        bg: '31ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 38%; left: 25%; width: 46%',
+            model: '',
+            solution: 'frankreich'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 43%; left: 29%; width: 40%',
+            model: '',
+            solution: 'griechenland'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 47.8%; left: 18%; width: 56%',
+            model: '',
+            solution: 'deutschland'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 52.6%; left: 26%; width: 44.5%',
+            model: '',
+            solution: 'brasilien'
+          },
+          {
+            identifier: 'five',
+            position: 'top: 57.5%; left: 24%; width: 47.3%',
+            model: '',
+            solution: 'marokko'
+          },
+          {
+            identifier: 'six',
+            position: 'top: 62.2%; left: 39.5%; width: 25%',
+            model: '',
+            solution: 'spanien'
+          },
+          {
+            identifier: 'seven',
+            position: 'top: 67%; left: 21%; width: 51.5%',
+            model: '',
+            solution: 'china'
+          },
+          {
+            identifier: 'eight',
+            position: 'top: 72%; left: 20%; width: 53%',
+            model: '',
+            solution: 'italien'
+          },
+          {
+            identifier: 'nine',
+            position: 'top: 76.7%; left: 24%; width: 47.2%',
+            model: '',
+            solution: 'russland'
+          }
+        ]
+      }
+    },
+    {
+      no: 32,
+      title: 'Hör zu und kreuze die richtige Antwort an!',
+      exTrigger: {
+        bottom: '3%',
+        top: '53%'
+      },
+      ex: {
+        name: 'mediumselect',
+        bg: '32ex',
+        data: [
+          {
+            identifier: 'one',
+            model: '',
+            position: 'top: 51.5%; height: 4%; position: absolute; width: 50%',
+            answerTrue: 'right: 51.5%',
+            answerFalse: 'right: 10.5%'
+          },
+          {
+            identifier: 'two',
+            model: '',
+            position: 'top: 61.7%; height: 4%; position: absolute; width: 50%',
+            answerFalse: 'right: 51.5%',
+            answerTrue: 'right: 10.5%'
+          },
+          {
+            identifier: 'three',
+            model: '',
+            position: 'top: 71.9%; height: 4%; position: absolute; width: 50%',
+            answerFalse: 'right: 51.5%',
+            answerTrue: 'right: 10.5%'
+          },
+          {
+            identifier: 'four',
+            model: '',
+            position: 'top: 82.1%; height: 4%; position: absolute; width: 50%',
+            answerTrue: 'right: 51.5%',
+            answerFalse: 'right: 10.5%'
+          },
+          {
+            identifier: 'five',
+            model: '',
+            position: 'top: 51.5%; right: 0; height: 4%; position: absolute; width: 50%',
+            answerTrue: 'right: 56.9%',
+            answerFalse: 'right: 15.8%'
+          },
+          {
+            identifier: 'six',
+            model: '',
+            position: 'top: 61.7%; right: 0; height: 4%; position: absolute; width: 50%',
+            answerTrue: 'right: 56.9%',
+            answerFalse: 'right: 15.8%'
+          },
+          {
+            identifier: 'seven',
+            model: '',
+            position: 'top: 71.9%; right: 0; height: 4%; position: absolute; width: 50%',
+            answerTrue: 'right: 56.9%',
+            answerFalse: 'right: 15.8%'
+          },
+          {
+            identifier: 'eight',
+            model: '',
+            position: 'top: 82.1%; right: 0; height: 4%; position: absolute; width: 50%',
+            answerFalse: 'right: 56.9%',
+            answerTrue: 'right: 15.8%'
+          }
+        ]
       }
     },
     {
@@ -228,6 +522,76 @@ module.exports = function() {
       }
     },
     {
+      no: 47,
+      exTrigger: {
+        bottom: '3%',
+        top: '56.5%'
+      },
+      ex: {
+        name: 'dnd',
+        bg: '',
+        solution: false,
+        data: [
+          {
+            rowID: 'one',
+            questionID: '1',
+            questionText: 'Wie heißt du?',
+            answerID: 'h',
+            answerText: 'Ich heiße Pauline.'
+          },
+          {
+            rowID: 'two',
+            questionID: '2',
+            questionText: 'Woher kommst du?',
+            answerID: 'a',
+            answerText: 'Ich komme aus Frankreich.'
+          },
+          {
+            rowID: 'three',
+            questionID: '3',
+            questionText: 'Wie alt bist du?',
+            answerID: 'c',
+            answerText: 'Ich bin vierzehn Jahre alt.'
+          },
+          {
+            rowID: 'four',
+            questionID: '4',
+            questionText: 'Wo wohnst du?',
+            answerID: 'b',
+            answerText: 'Ich wohne in Paris.'
+          },
+          {
+            rowID: 'five',
+            questionID: '5',
+            questionText: 'Was ist deine Lieblingsfarbe?',
+            answerID: 'f',
+            answerText: 'Gelb.'
+          },
+          {
+            rowID: 'six',
+            questionID: '6',
+            questionText: 'Magst du Pizza?',
+            answerID: 'd',
+            answerText: 'Ja, es ist mein Lieblingsessen.'
+          },
+          {
+            rowID: 'seven',
+            questionID: '7',
+            questionText: 'Kannst du Gitarre spielen?',
+            answerID: 'e',
+            answerText: 'Nein, das kann ich nicht.'
+          },
+          {
+            rowID: 'eight',
+            questionID: '8',
+            questionText: 'Hast du ein Haustier?',
+            answerID: 'g',
+            answerText: 'Ja, einen Hund.'
+          }
+        ]
+      }
+    },
+    {
       no: 49,
       static: 'pg49s1',
       staticTrigger: {
@@ -238,6 +602,229 @@ module.exports = function() {
       static2Trigger: {
         top: '14%',
         bottom: '55.5%'
+      }
+    },
+    {
+      no: 52,
+      static: 'pg52nicht',
+      staticTrigger: {
+        top: '66%',
+        bottom: '7%'
+      }
+    },
+    {
+      no: 56,
+      static: 'pg56ex8',
+      staticTrigger: {
+        top: '32%',
+        bottom: '4%'
+      }
+    },
+    {
+      no: 57,
+      static: 'pg57nicht',
+      staticTrigger: {
+        top: '66%',
+        bottom: '4%'
+      }
+    },
+    {
+      no: 59,
+      static: 'pg59s1',
+      staticTrigger: {
+        top: '11.5%',
+        bottom: '53%'
+      },
+      static2: 'pg59s2',
+      static2Trigger: {
+        top: '47%',
+        bottom: '4.5%'
+      }
+    },
+    {
+      no: 60,
+      static: 'pg60ex12',
+      staticTrigger: {
+        top: '13%',
+        bottom: '59%'
+      }
+    },
+    {
+      no: 61,
+      title: 'Was haben die Kinder genacht? Wann? Schreib es!',
+      exTrigger: {
+        bottom: '4.5%',
+        top: '59%'
+      },
+      ex: {
+        name: 'typer',
+        bg: '61ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 64.5%; left: 11%; width: 27%',
+            model: '',
+            solution: 'viertel vor acht'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 74%; left: 6%; width: 33%',
+            model: '',
+            solution: 'gegangen'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 64.5%; left: 42%; width: 25%',
+            model: '',
+            solution: 'vier uhr'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 74%; left: 37%; width: 32%',
+            model: '',
+            solution: 'gemacht'
+          },
+          {
+            identifier: 'five',
+            position: 'top: 64.5%; left: 72%; width: 28%',
+            model: '',
+            solution: 'halb acht'
+          },
+          {
+            identifier: 'six',
+            position: 'top: 74%; left: 68%; width: 32%',
+            model: '',
+            solution: 'gegessen'
+          }
+        ]
+      }
+    },
+    {
+      no: 63,
+      static: 'pg63ex4',
+      staticTrigger: {
+        top: '14%',
+        bottom: '35%'
+      }
+    },
+    {
+      no: 65,
+      title: 'Ergänze!',
+      exTrigger: {
+        bottom: '27.5%',
+        top: '12%'
+      },
+      ex: {
+        name: 'typer',
+        bg: '65ex',
+        data: [
+          {
+            identifier: 'one',
+            position: 'top: 52%; left: 35%; width: 25%',
+            model: '',
+            solution: 'schneller'
+          },
+          {
+            identifier: 'two',
+            position: 'top: 52%; left: 61%; width: 25%',
+            model: '',
+            solution: 'am schnellsten'
+          },
+          {
+            identifier: 'three',
+            position: 'top: 93.5%; left: 38%; width: 21%',
+            model: '',
+            solution: 'länger'
+          },
+          {
+            identifier: 'four',
+            position: 'top: 93.5%; left: 62%; width: 25%',
+            model: '',
+            solution: 'am längsten'
+          }
+        ]
+      }
+    },
+    {
+      no: 66,
+      static: 'pg66ostern',
+      staticTrigger: {
+        top: '14%',
+        bottom: '5%'
+      }
+    },
+    {
+      no: 68,
+      title: 'Ergänze!',
+      exTrigger: {
+        bottom: '68.5%',
+        top: '6%'
+      },
+      ex: {
+        name: 'mediumselect',
+        bg: '68ex',
+        data: [
+          {
+            identifier: 'one',
+            model: '',
+            position: 'margin-top: 25.3%; height: 4.3%',
+            answerTrue: 'right: 34.5%',
+            answerFalse: 'right: 26.3%'
+          },
+          {
+            identifier: 'two',
+            model: '',
+            position: 'margin-top: 1.4%; height: 4.3%',
+            answerFalse: 'right: 34.5%',
+            answerTrue: 'right: 26.3%'
+          },
+          {
+            identifier: 'three',
+            model: '',
+            position: 'margin-top: 1.3%; height: 4.3%',
+            answerFalse: 'right: 34.5%',
+            answerTrue: 'right: 26.3%'
+          },
+          {
+            identifier: 'four',
+            model: '',
+            position: 'margin-top: 1.3%; height: 4.3%',
+            answerTrue: 'right: 34.5%',
+            answerFalse: 'right: 26.3%'
+          },
+          {
+            identifier: 'five',
+            model: '',
+            position: 'margin-top: 1.5%; height: 4.3%',
+            answerFalse: 'right: 34.5%',
+            answerTrue: 'right: 26.3%'
+          },
+          {
+            identifier: 'six',
+            model: '',
+            position: 'margin-top: 1.4%; height: 4.3%',
+            answerTrue: 'right: 34.5%',
+            answerFalse: 'right: 26.3%'
+          }
+        ]
+      }
+    },
+    {
+      no: 81,
+      static: 'pg81ex15s1',
+      staticTrigger: {
+        top: '9%',
+        bottom: '48%'
+      },
+      static2: 'pg81ex15s2',
+      static2Trigger: {
+        top: '52%',
+        bottom: '30%'
+      },
+      static3: 'pg81ex15s3',
+      static3Trigger: {
+        top: '70%',
+        bottom: '3%'
       }
     }
   ]
